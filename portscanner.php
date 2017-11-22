62e96ee90a8e4a6a1a77750c0fdabb3249a3713f
Vasil Anagnostos <vasil.a@me.com>
2017-11-22 08:21:56 +0200

    Refactor

--- portscanner.php
+++ portscanner.php
@@ -1,166 +1,288 @@
 <?php
 require "db.php";
 
-abstract class PortScanner
+/**
+ * Interface ScannerInterface
+ */
+interface ScannerInterface
 {
-	protected $idServer;
+    /**
+     * @return string
+     */
+    public function getIdServer();
 
-	protected $socket;
-
-	protected $socketAddress;
-
-	protected $socketPort;
-
-	protected $socketDomain = AF_INET;
-
-	protected $socketType = SOCK_STREAM;
-
-	protected $socketProtocol = SOL_TCP;
-
-	protected $socketMessage;
-
-	protected $expectedResult;
-
-	/**
-	 * PortScanner constructor.
-	 *
-	 * @param $idServer
-	 * @param $address
-	 * @param $port
-	 * @param $message
-	 * @param $expectedResult
-	 */
-	function __construct($idServer, $address, $port, $message, $expectedResult)
-	{
-		$this->idServer = $idServer;
-		$this->socketAddress = $address;
-		$this->socketPort = $port;
-		$this->socketMessage = $message;
-		$this->expectedResult = $expectedResult;
-	}
-
-	/**
-	 * Creates the socket
-	 */
-	public function socketCreate()
-	{
-		$this->socket = socket_create($this->socketDomain, $this->socketType, $this->socketProtocol);
-	}
-
-	/**
-	 * @return bool
-	 */
-	public function socketConnect()
-	{
-		$this->socketCreate();
-
-		return socket_connect($this->socket, $this->socketAddress, $this->socketPort);
-	}
-
-	/**
-	 * @return int
-	 */
-	public function socketWrite()
-	{
-		return socket_write($this->socket, $this->socketMessage, strlen($this->socketMessage));
-	}
-
-	/**
-	 * @return string
-	 */
-	public function socketRead()
-	{
-		return socket_read($this->socket, 2048);
-	}
-
-	public function socketClose()
-	{
-		return socket_close($this->socket);
-	}
-
-	/**
-	 * @return mixed
-	 */
-	public function getSocket()
-	{
-		return $this->socket;
-	}
-
-	/**
-	 * @return mixed
-	 */
-	public function getIdServer()
-	{
-		return $this->idServer;
-	}
-
-	/**
-	 * @param $readResponse
-	 *
-	 * @return bool
-	 */
-	public function validateResponse($readResponse)
-	{
-		return preg_match("/" . $this->expectedResult . " /", $readResponse) == 1 ? true : false;
-	}
+    /**
+     * @return string
+     */
+    public function runScan();
 }
 
 /**
- * Class SocketTcp
+ * Class PortScanner
  */
-class SocketTcp extends PortScanner
+abstract class PortScanner implements ScannerInterface
 {
-	/**
-	 * SocketTcp constructor.
-	 *
-	 * @param $idServer
-	 * @param $address
-	 * @param $port
-	 * @param $expectedResult
-	 */
-	function __construct($idServer, $address, $port, $expectedResult)
-	{
-		$socketMessage = "HEAD / HTTP/1.1\r\n";
-		$socketMessage .= "Host: " . $address . "\r\n";
-		$socketMessage .= "Connection: Close\r\n\r\n";
+    /**
+     * @var string
+     */
+    protected $idServer;
 
-		parent::__construct($idServer, $address, $port, $socketMessage, $expectedResult);
-	}
+    /**
+     * @var resource
+     */
+    protected $socket;
+
+    /**
+     * @var string
+     */
+    protected $socketAddress;
+
+    /**
+     * @var int
+     */
+    protected $socketPort;
+
+    /**
+     * @var int
+     */
+    protected $socketDomain = AF_INET;
+
+    /**
+     * @var int
+     */
+    protected $socketType = SOCK_STREAM;
+
+    /**
+     * @var int
+     */
+    protected $socketProtocol = SOL_TCP;
+
+    /**
+     * @var string
+     */
+    protected $socketMessage;
+
+    /**
+     * @var string
+     */
+    protected $expectedResult;
+
+    /**
+     * PortScanner constructor.
+     *
+     * @param $idServer
+     * @param $address
+     * @param $port
+     * @param $expectedResult
+     * @param $message
+     */
+    public function __construct($idServer, $address, $port, $expectedResult, $message = null)
+    {
+        $this->idServer = $idServer;
+        $this->socketAddress = $address;
+        $this->socketPort = $port;
+        $this->expectedResult = $expectedResult;
+        $this->socketMessage = $message;
+    }
+
+    /**
+     * @return string
+     */
+    public function runScan()
+    {
+        if (!$this->socketConnect()) {
+            return 'Connection failed: ' . socket_strerror(socket_last_error());
+        }
+
+        $this->socketWrite();
+
+        $result = $this->socketRead();
+
+        $return = $this->validateResponse($result) ? null : 'Invalid response: ' . $result;
+
+        $this->socketClose();
+
+        return $return;
+    }
+
+    /**
+     * Creates the socket
+     */
+    public function socketCreate()
+    {
+        $this->socket = socket_create($this->socketDomain, $this->socketType, $this->socketProtocol);
+
+        socket_set_option(
+            $this->socket,
+            SOL_SOCKET,
+            SO_RCVTIMEO,
+            ['sec' => 10, 'usec' => 0]
+        );
+    }
+
+    /**
+     * @return bool
+     */
+    public function socketConnect()
+    {
+        $this->socketCreate();
+
+        return socket_connect($this->socket, $this->socketAddress, $this->socketPort);
+    }
+
+    /**
+     * @return int
+     */
+    public function socketWrite()
+    {
+        if ($this->socketMessage) {
+            return socket_write($this->socket, $this->socketMessage, strlen($this->socketMessage));
+        }
+
+        return 0;
+    }
+
+    /**
+     * @return string
+     */
+    public function socketRead()
+    {
+        return socket_read($this->socket, 8192);
+    }
+
+    /**
+     * @return void
+     */
+    public function socketClose()
+    {
+        socket_close($this->socket);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSocket()
+    {
+        return $this->socket;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIdServer()
+    {
+        return $this->idServer;
+    }
+
+    /**
+     * @param $readResponse
+     *
+     * @return bool
+     */
+    public function validateResponse($readResponse)
+    {
+        return preg_match("/" . $this->expectedResult . " /", $readResponse) == 1 ? true : false;
+    }
 }
 
 /**
- * Class SocketHandler
+ * Class MailServerScanner
  */
-class SocketHandler
+class MailServerScanner extends PortScanner
 {
-	protected $dbAccess;
+    /**
+     * SocketTcp constructor.
+     *
+     * @param $idServer
+     * @param $address
+     * @param $port
+     * @param $expectedResult
+     */
+    public function __construct($idServer, $address, $port, $expectedResult)
+    {
+        parent::__construct($idServer, $address, $port, null, $expectedResult);
+    }
 
-	protected $idServer;
+    /**
+     * @return string
+    public function runScan(){
+     * change the logic completely if you like ...
+     * for example mail server does not require socketWrite call
+     * }
+     */
+}
 
-	/**
-	 * SocketHandler constructor.
-	 *
-	 * @param $dbAccess
-	 * @param $idServer
-	 */
-	function __construct($dbAccess, $idServer)
-	{
-		$this->dbAccess = $dbAccess;
-		$this->idServer = $idServer;
-	}
+/**
+ * Class WebServerScanner
+ */
+class WebServerScanner extends PortScanner
+{
+    /**
+     * SocketTcp constructor.
+     *
+     * @param $idServer
+     * @param $address
+     * @param $port
+     * @param $expectedResult
+     */
+    public function __construct($idServer, $address, $port, $expectedResult)
+    {
+        $socketMessage = "HEAD / HTTP/1.1\r\n";
+        $socketMessage .= "Host: " . $address . "\r\n";
+        $socketMessage .= "Connection: Close\r\n\r\n";
 
-	/**
-	 * Writes the response into the DataBase
-	 */
-	public function writeResponseInvalid()
-	{
-		return $this->dbAccess->execute("INSERT INTO server_results (id_server, result) VALUES (" . $this->idServer . ", 'Invalid response')");
-	}
+        parent::__construct($idServer, $address, $port, $expectedResult, $socketMessage);
+    }
+}
 
-	public function writeResponseInvalidHost()
-	{
-		return $this->dbAccess->execute("INSERT INTO server_results (id_server, result) VALUES (" . $this->idServer . ", '" . socket_strerror(socket_last_error()) . "')");
-	}
+/**
+ * Class FtpServerScanner
+ */
+class FtpServerScanner extends MailServerScanner{
+
+}
+
+/**
+ * Class StorageHandler
+ */
+class StorageHandler
+{
+    /**
+     * @var Database
+     */
+    protected $dbAccess;
+
+    /**
+     * SocketHandler constructor.
+     *
+     * @param $dbAccess
+     */
+    public function __construct(Database $dbAccess)
+    {
+        $this->dbAccess = $dbAccess;
+    }
+
+    /**
+     * Writes the response into the DataBase
+     * @param $socketId
+     * @param $message
+     * @return mixed
+     */
+    public function writeResponseInvalid($socketId, $message)
+    {
+        return $this->dbAccess->execute(
+            "INSERT INTO server_results (id_server, result) " .
+            "VALUES (" . $socketId . ", '" . $message . "')" // FIXME this should be quoted!
+        );
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAllServers()
+    {
+        return $this->dbAccess->getAll('SELECT * from server_list');
+    }
 }
 
 /**
@@ -168,78 +290,78 @@ class SocketHandler
  */
 class ScanServers
 {
-	protected $dbAccess;
+    /**
+     * @var DataBase
+     */
+    protected $dbAccess;
 
-	protected $servers = [];
+    /**
+     * @var StorageHandler
+     */
+    protected $storageHandler;
 
-	protected $sockets = [];
+    /**
+     * @var array
+     */
+    protected $sockets = [];
 
-	protected $socket;
+    /**
+     * @var array
+     */
+    protected $scanners = [
+        'web' => WebServerScanner::class,
+        'mail' => MailServerScanner::class,
+        'ftp' => FtpServerScanner::class,
+    ];
 
-	function getAllServers()
-	{
-		$this->dbAccess = new Database('localhost', 'root', '', 'portscanner', '3306');
-		return $this->servers = $this->dbAccess->getAll('SELECT * from server_list');
-	}
-	/**
-	 * Creates an array of sockets
-	 */
-	function getAllSockets()
-	{
-		foreach ($this->servers as $server)
-		{
-			$this->sockets[] = new SocketTcp(
-				$server[ 'idserver' ],
-				$server[ 'address' ],
-				$server[ 'port' ],
-				$server[ 'expected_result' ]
-			);
-		}
-		return $this->sockets;
-	}
+    /**
+     * ScanServers constructor.
+     * @param DataBase $dbAccess
+     */
+    public function __construct(DataBase $dbAccess)
+    {
+        $this->dbAccess = $dbAccess;
+        $this->storageHandler = new StorageHandler($this->dbAccess);
+    }
 
-	/**
-	 * Runs the scan in all the servers from the database
-	 */
-	function runScan()
-	{
-		/**
-		 * Scans the servers and writes invalid responses in the db
-		 */
-		$this->getAllServers();
+    /**
+     * Runs the scan in all the servers from the database
+     */
+    public function runScan()
+    {
+        foreach ($this->storageHandler->getAllServers() as $server) {
+            $socket = $this->scannerFactory($server);
 
-		foreach ($this->getAllSockets() as $socket)
-		{
-			$socketId = $socket->getIdServer();
-			$socketHandler = new SocketHandler($this->dbAccess, $socketId);
+            if (false != ($issueDetected = $socket->runScan())) {
+                $socketId = $socket->getIdServer();
+                $this->storageHandler->writeResponseInvalid($socketId, $issueDetected);
+                echo "Issue detected with server " . $socketId . ": " . $issueDetected . PHP_EOL;
+            }
+        }
 
-			@$exists = $socket->socketConnect();
+        echo "Scan completed\n";
+        $this->dbAccess->disconnect();
+    }
 
-			if ($exists)
-			{
-				$socket->socketWrite();
-				$result = $socket->socketRead();
+    /**
+     * Creates scanner instance depending on config
+     * @param array $config
+     * @return ScannerInterface
+     */
+    protected function scannerFactory(array $config)
+    {
+        $scanner = $this->scanners[$config['type']];
 
-				$status = $socket->validateResponse($result);
-
-				if (!$status)
-				{
-					$socketHandler->writeResponseInvalid();
-					echo "Server id: " . $socketId . " Invalid Response \n";
-				}
-			}
-			else
-			{
-				$socketHandler->writeResponseInvalidHost();
-				echo "Server id: " . $socketId . " Invalid Host\n";
-			}
-
-			$socket->socketClose();
-		}
-
-		echo "Data stored in database\n";
-		$this->dbAccess->disconnect();
-	}
+        return new $scanner(
+            $config['idserver'],
+            $config['address'],
+            $config['port'],
+            $config['expected_result']
+        );
+    }
 }
 
-(new ScanServers())->runScan();
+(new ScanServers(
+// Db access is normally in a config file
+    new Database('localhost', 'root', 'alabala12', 'portscanner', '3306')
+))->runScan();
